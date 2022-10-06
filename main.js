85b26e4a0be6ad1c3d015f712463bcc7e64c8779
httpat <patriciavbalduci@icloud.com>
2022-10-06 02:04:38 -0300

    consertando bugs

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const inputNotaAtividade = document.getElementById('nota-atividade')
 
     let linha = '<tr>';
     linha += `<td> ${inputNomeAtividade.value}</td>`;
-    linha += `<td> ${inputNotaAtividade.value}<td>`;
-    linha += `<td> ${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}<td>`;
+    linha += `<td> ${inputNotaAtividade.value}</td>`;
+    linha += `<td> ${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
     linha += '<tr>';
 
     linhas += linha;
