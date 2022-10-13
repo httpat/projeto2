57ed2cc1de71e488c61b8eec99280f57ed780f14
httpat <patriciavbalduci@icloud.com>
2022-10-13 17:19:04 -0300

    Excluindo imagens não utilizadas

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const form = document.getElementById('form-atividade')
-const imgAprovado = '<img src="./aprovado.png"/>';
-const imgReprovado = '<img src="./reprovado.png"/>'; 
 let linhas = ''
 form.addEventListener('submit', function(e) {
 e.preventDefault();
@@ -10,7 +8,6 @@ const inputNotaAtividade = document.getElementById('nota-atividade')
     let linha = '<tr>';
     linha += `<td> ${inputNomeAtividade.value}</td>`;
     linha += `<td> ${inputNotaAtividade.value}</td>`;
-    linha += `<td> ${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
     linha += '<tr>';
 
     linhas += linha;
